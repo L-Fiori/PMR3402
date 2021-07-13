29b684bdbc514c9aaec48d78d9a4d4033c2b60b9
vinidm <vinidmc@hotmail.com>
2021-07-13 14:10:17 -0300

    Implementar módulo Bluetooth
    Implementar módulo Bluetooth

---
+++ Teste_Classes/Teste_Classes.ino
@@ -0,0 +1,11 @@
+#include "modulo_bluetooth.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  ModuloBluetooth blu(2,3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
